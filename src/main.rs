347bf9e7bca9e367697acd616450ceb3ad61ae03
llcm <1280955870@qq.com>
2025-05-16 23:58:46 +0800

    feat: enhance main function to support SQL execution from a file or REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use simple_db::execute_sql;
+
 /// 简易 Rust 数据库程序
 ///
 /// 这个程序实现了一个简单的数据库系统，支持基本的 SQL 操作，
@@ -13,6 +15,20 @@ mod utils;
 ///
 /// 启动一个交互式的 REPL（读取-求值-打印-循环）环境，让用户可以执行 SQL 命令
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    repl::run_repl()?;
+    let args: Vec<String> = std::env::args().collect();
+    match args.len() {
+        1 => repl::run_repl()?,
+        2 => {
+            let filename = args[1].clone();
+            let path = std::path::Path::new(&filename);
+            if path.exists() {
+                let sql = std::fs::read_to_string(path)?;
+                execute_sql(&sql);
+            } else {
+                return Err("File note found".into());
+            }
+        }
+        _ => return Err("Invalid arguments".into()),
+    }
     Ok(())
 }
